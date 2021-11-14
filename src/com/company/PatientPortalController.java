5f4e8ee6a3009f498509b6572381f3c177d57f19
Madeline <72108857+balynch3@users.noreply.github.com>
2021-11-13 20:26:03 -0700

    Added basic username and password change functionality + changing doctors
    1. Added ability to Change Doctor. Needs database implementation.
    
    2. Added ability to change username and password. Needs database implementation.

--- src/com/company/PatientPortalController.java
+++ src/com/company/PatientPortalController.java
@@ -3,12 +3,14 @@ package com.company;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -20,6 +22,10 @@ public class PatientPortalController {
     private Parent PatientPortalPane;
     private int userID;
     private FXMLLoader loader;
+    //The current values are a placeholder. They should be obtained from the database using the userID
+    //in the constructor. Might replace with a Patient object constructed using the userID.
+    private String username = "username";
+    private String password = "password";
 
     //The Patient Info Tab objects:
     //Might end up changing this to hold a Record object type so that we can display date and name in the
@@ -61,7 +67,7 @@ public class PatientPortalController {
     @FXML
     private PasswordField verifyInfoEditField;
 
-    //The Edit Account Tab objects:
+    //The Edit Account (change username/pass) Tab objects:
     @FXML
     private Label UpdateAccountStatusLabel;
     @FXML
@@ -72,6 +78,26 @@ public class PatientPortalController {
     private TextField NewUsernameField;
     @FXML
     private PasswordField NewPasswordField;
+    @FXML
+    private Label OldUsernameStatusLabel;
+    @FXML
+    private Label OldPasswordStatusLabel;
+    @FXML
+    private Label NewUsernameStatusLabel;
+    @FXML
+    private Label NewPasswordStatusLabel;
+
+    //The Change Doctor Tab
+    @FXML
+    private Label ChangeDoctorStatusLabel;
+    @FXML
+    private ListView<String> ChangeDoctorList;
+    @FXML
+    private Label CurrentDoctorLabel;
+    @FXML
+    private Label SelectedDoctorLabel;
+    @FXML
+    private PasswordField verifyDoctorChangeField;
 
     //The Message Tab objects:
     @FXML
@@ -86,6 +112,7 @@ public class PatientPortalController {
     public PatientPortalController(int userID, FXMLLoader loader) {
         this.userID = userID;
         this.loader = loader;
+        //Set username and password from database.
     }
 
     //This function runs when the pane is loaded.
@@ -96,19 +123,41 @@ public class PatientPortalController {
         System.out.println("\tLoader: " + loader);
         System.out.println("\tuserID: " + userID);
 
-        ObservableList<String> data = FXCollections.observableArrayList("This is just a temporary data set",
-                "We are gonna get this data from the database when it gets added", "Third Option");
-        PatientRecordList.setItems(data);
+        //Set up Patient Record List
+        ObservableList<String> PatientRecordData = FXCollections.observableArrayList(
+                "This is just a temporary data set",
+                "We are gonna get this data from the database when it gets added",
+                "Third Option");
+        PatientRecordList.setItems(PatientRecordData);
 
-        //This code is used to create a Listener that listens to the selected value
-        //of the ListView object. We are going to use this for showing text, etc.
-        //Here we are setting a listener to listen to the selected property, which is a value
-        //that changes whenever an item is selected.
+        /*
+         * This code is used to create a Listener that listens to the selected value
+         * of the ListView object. We are going to use this for showing text, etc.
+         * Here we are setting a listener to listen to the selected property, which is a value
+         * that changes whenever an item is selected.
+         */
         PatientRecordList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
-            System.out.println("Selected item: " + observable.getValue());
+            System.out.println("Record List selected item: " + observable.getValue());
             PatientRecordTextArea.setText(observable.getValue());
             //System.out.println("Old value: " + oldValue + "\nNew Value: " + newValue);
         });
+
+        //Set up Doctor List
+        ObservableList<String> PatientDoctorList = FXCollections.observableArrayList(
+                "Dr. Temp Name",
+                "Dr. Other Temp Name",
+                "Dr. Third Temp Name");
+        ChangeDoctorList.setItems(PatientDoctorList);
+        ChangeDoctorList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("Doctor List selected item: " + observable.getValue());
+            SelectedDoctorLabel.setText(observable.getValue());
+            //System.out.println("Old value: " + oldValue + "\nNew Value: " + newValue);
+        });
+
+        //Set up Messaging list
+        //...
+
+        //Set up Labels using database info.
     }
 
     public Parent getParent() {
@@ -130,7 +179,7 @@ public class PatientPortalController {
                 Objects.equals(InsuranceField.getText(), "")) {
             UpdateInfoStatusLabel.setTextFill(Color.RED);
             UpdateInfoStatusLabel.setText("A field is empty.");
-        } else if (!Objects.equals(verifyInfoEditField.getText(), "password")) { //temp password
+        } else if (!Objects.equals(verifyInfoEditField.getText(), password)) {
             UpdateInfoStatusLabel.setTextFill(Color.RED);
             UpdateInfoStatusLabel.setText("Incorrect Password");
             //System.out.println("Password: " + verifyInfoEditField.getText());
@@ -147,14 +196,116 @@ public class PatientPortalController {
             PatientInsuranceLabel.setText(InsuranceField.getText());
         }
         verifyInfoEditField.setText("");
-
-        //We also need to update the TextArea on the other tab to be correct.
     }
 
     @FXML
     protected void onUpdateAccountClick(ActionEvent event) {
-        UpdateAccountStatusLabel.setText("Your Account information was updated.");
-        UpdateAccountStatusLabel.setTextFill(Color.LIMEGREEN);
+        if (OldUsernameField.getText().equals("") ||
+                OldPasswordField.getText().equals("") ||
+                NewUsernameField.getText().equals("") ||
+                NewPasswordField.getText().equals("")) {
+            UpdateAccountStatusLabel.setTextFill(Color.RED);
+            UpdateAccountStatusLabel.setText("All fields must be completed");
+        } else if (!OldUsernameField.getText().equals(username)) {
+            UpdateAccountStatusLabel.setTextFill(Color.RED);
+            UpdateAccountStatusLabel.setText("Incorrect Username");
+        } else if (!OldPasswordField.getText().equals(password)) {
+            UpdateAccountStatusLabel.setTextFill(Color.RED);
+            UpdateAccountStatusLabel.setText("Incorrect Password");
+            //This check will be replaced by checking if the new username equals any current username
+            //and **not  the current user's**. This is just a temporary test.
+        } else if (NewUsernameField.getText().equals(username)) {
+            UpdateAccountStatusLabel.setTextFill(Color.RED);
+            UpdateAccountStatusLabel.setText("New Username must be unique");
+        } else if (NewPasswordField.getText().length() < 8) {
+            UpdateAccountStatusLabel.setTextFill(Color.RED);
+            UpdateAccountStatusLabel.setText("New Username must be unique");
+        } else {
+            //Replace with database query and a change to the Patient object (when we add it)
+            this.username = NewUsernameField.getText();
+            this.password = NewPasswordField.getText();
+            OldUsernameField.setText("");
+            OldPasswordField.setText("");
+            NewUsernameField.setText("");
+            NewPasswordField.setText("");
+            UpdateAccountStatusLabel.setTextFill(Color.LIMEGREEN);
+            UpdateAccountStatusLabel.setText("Account updated");
+        }
+    }
+
+    //Handles status messages for the Change username/password tab fields
+    @FXML
+    protected void onOldUsernameFieldType(Event event) {
+        if (!OldUsernameField.getText().equals(username)) {
+            OldUsernameStatusLabel.setText("Incorrect Username");
+            OldUsernameStatusLabel.setTextFill(Color.RED);
+        } else {
+            OldUsernameStatusLabel.setTextFill(Color.LIMEGREEN);
+            OldUsernameStatusLabel.setText("Username is correct");
+        }
+    }
+
+    @FXML
+    protected void onOldPasswordFieldType(Event event) {
+        if (!OldPasswordField.getText().equals(password)) {
+            OldPasswordStatusLabel.setText("Incorrect Password");
+            OldPasswordStatusLabel.setTextFill(Color.RED);
+        } else {
+            OldPasswordStatusLabel.setTextFill(Color.LIMEGREEN);
+            OldPasswordStatusLabel.setText("Password is correct");
+        }
+    }
+
+    @FXML
+    protected void onNewUsernameFieldType(Event event) {
+        //We would replace this condition to be a check for if the username is used
+        //by any other patient account and not by the current patient account.
+        if (NewUsernameField.getText().equals("")) {
+            NewUsernameStatusLabel.setTextFill(Color.RED);
+            NewUsernameStatusLabel.setText("Must supply a username");
+        } else if (NewUsernameField.getText().equals(username)) {
+            NewUsernameStatusLabel.setTextFill(Color.RED);
+            NewUsernameStatusLabel.setText("Username already in use");
+        } else {
+            NewUsernameStatusLabel.setTextFill(Color.LIMEGREEN);
+            NewUsernameStatusLabel.setText("Username is unique");
+        }
+        //This runs for every character typed. Hopefully that isn't too inefficient. It is good for a live update
+    }
+
+    @FXML
+    protected void onNewPasswordFieldType(Event event) {
+        if (NewPasswordField.getText().equals("")) {
+            NewPasswordStatusLabel.setTextFill(Color.RED);
+            NewPasswordStatusLabel.setText("Must supply a valid password");
+        } else if (NewPasswordField.getText().length() < 8) {
+            NewPasswordStatusLabel.setTextFill(Color.RED);
+            NewPasswordStatusLabel.setText("Password too short. Must be 8 characters or more");
+        } else if (NewPasswordField.getText().length() < 12) {
+            NewPasswordStatusLabel.setTextFill(Color.ORANGE);
+            NewPasswordStatusLabel.setText("Password is valid, but a longer one is suggested");
+        } else {
+            NewPasswordStatusLabel.setTextFill(Color.LIMEGREEN);
+            NewPasswordStatusLabel.setText("Strong, valid Password");
+        }
+    }
+
+    @FXML
+    protected void onChangeDoctorClick(ActionEvent event) {
+        if (verifyDoctorChangeField.getText().equals(password)) {
+            if (ChangeDoctorList.getSelectionModel().getSelectedItem() == null) {
+                ChangeDoctorStatusLabel.setTextFill(Color.RED);
+                ChangeDoctorStatusLabel.setText("Nothing selected.");
+            } else {
+                ChangeDoctorStatusLabel.setTextFill(Color.LIMEGREEN);
+                ChangeDoctorStatusLabel.setText("Your doctor has been updated");
+                CurrentDoctorLabel.setText(ChangeDoctorList.getSelectionModel().getSelectedItem());
+            }
+        } else {
+            ChangeDoctorStatusLabel.setTextFill(Color.RED);
+            ChangeDoctorStatusLabel.setText("Incorrect Password");
+        }
+        verifyDoctorChangeField.setText("");
     }
 
     @FXML
