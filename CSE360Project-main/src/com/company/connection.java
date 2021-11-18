a02ed326b020f1cc4c8a1e38da9b75a71b990970
supreeth999 <sthota19@asu.edu>
2021-11-17 19:35:52 -0700

    created connection

---
+++ CSE360Project-main/src/com/company/connection.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connection {
+    Connection conn=null;
+    public Connection getdbconnection()
+    {
+        try {
+            this.conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "charishma@123");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
